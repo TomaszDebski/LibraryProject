2c65a734eed2ac1715ee24157ff4bcd989fb3781
tsuisou <tsuisou1@gmail.com>
2016-07-10 12:35:08 +0200

    new update

---
+++ IdeaProjects/LibraryProject/src/main/java/pl/library/LibraryProjectApplication.java
@@ -0,0 +1,12 @@
+package pl.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryProjectApplication.class, args);
+	}
+}
